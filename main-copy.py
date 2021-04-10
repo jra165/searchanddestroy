6e5ecab222b7650e0e3647c9afac0416483c5225
JanetZhang8 <44734856+JanetZhang8@users.noreply.github.com>
2021-04-10 11:50:01 -0400

    Update main-copy.py

--- main-copy.py
+++ main-copy.py
@@ -15,8 +15,248 @@ from queue import PriorityQueue, Queue
 from typing import List, Tuple
 
 
-class Landscape:
+terrainDict = {
+    1: 0.1,
+    2: 0.3,
+    3: 0.7,
+    4: 0.9
+}
 
+def createEnv(initial, dimension):
+    env = [[initial]*dimension]*dimension
+    for x in range(dimension):
+        for y in range(dimension):
+            env[x][y] = terrainDict[random.randint(1, 4)]
+    return env
+
+env = createEnv(0, 10)
+
+
+class Agent:
+    def __init__(self, dimension):
+        self.dim = dimension
+        self.beliefState = createEnv(1/(dimension**2), dimension)
+        self.probabilities = PriorityQueue()
+        for i in range(dimension):
+            for j in range(dimension):
+                self.probabilities.put(-self.beliefState[i][j], (i,j))
+        self.target = [random.randint(0, dimension-1), random.randint(0,dimension-1)]
+        self.observations = createEnv(0, dimension)
+        self.memory = Queue(10)
+        return
+
+SimpleAgent = Agent(10)
+for i in range(10):
+    for j in range(10):
+        SimpleAgent.probabilities.put((-1*SimpleAgent.beliefState[i][j], (i,j)))
+
+def probabilitiesUpdate(Agent, tile, oldProb, part1):
+    newQueue = PriorityQueue()
+    probSum = 0
+    while not Agent.probabilities.empty():
+        curr = Agent.probabilities.get()
+        if curr[1] == tile:
+            Agent.beliefState[curr[1][0]][curr[1][1]] = Agent.probabilitiesCalculate(curr, tile, oldProb, "observed")
+        else: 
+            Agent.beliefState[curr[1][0]][cur[1][1]] = Agent.probabilitiesCalculate(curr, tile, oldProb, "other")
+        probSum += Agent.beliefState[curr[1][0]][curr[1][1]]
+    factor = 1/probSum
+    for i in range(Agent.dimension):
+        for j in range(Agent.dimension):
+            Agent.beliefState[i][j] = Agent.beliefState[i][j] * factor
+            if part1: 
+                newQueue.put(((calcDistance(tile, (i,j)+1))/Agent.beliefState[i][j], (i,j)))
+            else:
+                newQueue.put((0-(Agent.beliefState[i][j]), (i,j)))
+
+def probabilitiesUpdateImproved(env, Agent, tile, oldProb, part1):
+    newQueue = PriorityQueue()
+    probSum = 0
+    while not Agent.probabilities.empty():
+        curr = Agent.probabilities.get()
+        if curr[1] == tile:
+            Agent.beliefState[curr[1][0]][curr[1][1]] = calcProbs(env, Agent, curr, tile, oldProb, True)
+        else: 
+            Agent.beliefState[curr[1][0]][cur[1][1]] = calcProbs(env, Agent, curr, tile, oldProb, False)
+        probSum += Agent.beliefState[curr[1][0]][curr[1][1]]
+    factor = 1/probSum
+    for i in range(Agent.dim):
+        for j in range(Agent.dim):
+            Agent.beliefState[i][j] = Agent.beliefState[i][j] * factor
+            if part1: 
+                newQueue.put(((calcDist(tile, (i,j)+1))/Agent.beliefState[i][j], (i,j)))
+            else:
+                newQueue.put((calcDist(tile,(i,j))+1/Agent.beliefState[i][j]+Agent.observations[i][j], (i,j)))
+
+def calcProbs(env, agent, curr, tile, searched, old):
+    falseNeg = env[tile[0]][tile[1]]
+    oldBelief = abs(agent.beliefState[curr[1][0]][curr[1][1]])
+    searched = abs(searched)
+    if old:
+        newBelief = falsNeg * searched / (1- searched + falseNeg)
+    else:
+        newBelief = oldBelief / (1- searched + falseNeg)
+    return newBelief
+
+def searchTile(env, Agent, tile):
+    if curr == Agent.target:
+        falseNeg = random.uniform(0,1)
+        if falseNeg > env[curr[9]][curr[1]]:
+            return True
+    return False
+
+def updatePart2(env, Agent, tile):
+    if Agent.memory.qsize() == 10:
+        tile, isClose = Agent.memory.get()
+        factor = 1
+        if not isClose:
+            factor = -1
+        factor *= -1
+        for i in range(-5, 6):
+            for j in range(-5,6):
+                if 0 <= tile[0] + i < Agent.dim and 0<= tile[i] + j < Agent.dim and calcDist(tile, (tile[0]+i, tile[1]+j)) <= 5:
+                    Agent.observations[tile[0]+i][tile[1]+j] += factor*0.25
+
+    else:
+        distfromTarget = calcDist(curr, Agent.target)
+        factor = 1
+        if distfromTarget > 5:
+            factor = -1
+        for i in range(-5, 6):
+            for j in range(-5,6):
+                if 0 <= tile[0] + i < Agent.dim and 0<= tile[i] + j < Agent.dim and calcDist(tile, (tile[0]+i, tile[1]+j)) <= 5:
+                    Agent.observations[tile[0]+i][tile[1]+j] += factor*0.25
+
+def checkTile(env, Agent, curr):
+    if curr == Agent.target:
+        falseNeg = random.uniform(0,1)
+        if falseNeg > env[curr[0]][curr[1]]:
+            return True
+    return False
+
+
+
+def startSearch(env, Agent, param, part2):
+    tileFound = False
+    tilePrev = None
+    tilesSearched = 1
+    tilesTravelled = 0
+
+    # Assigns initial probability values based on likelihood target is seen
+    if param == "Rule2" or param == "Agent2" or param == "Agent3":
+        newQueue = PriorityQueue()
+        probSum = 0
+        for i in range(Agent.dim):
+            for j in range(Agent.dim):
+                Agent.beliefState[i][j] *= (1 - env[i][j])
+                probSum += Agent.beliefState[i][j]
+        Agent.probabilities = newQueue
+    if param == "Rule1" or param == "Agent1":
+        a = random.randint(0, Agent.dim - 1)
+        b = random.randint(0, Agent.dim - 1)
+        curr = [a, b]
+        tileFound = checkTile(env, Agent, curr)
+
+        # Find whether we're 5 distance if target not found for part 2
+        if not tileFound and part2:
+            updatePart2(env, Agent, curr)
+        probabilitiesUpdate(Agent, curr, -1/(Agent.dim**2), not part2)
+        tilePrev = (0, curr)
+    
+    # Pick optimal flat terrain cell as the initial random cell
+    elif version == "improved":
+        #pick improved
+        minScore = 1
+        optimal = None
+        for i in range(Agent.dim):
+            for j in range(Agent.dim):
+                if env[i][j] == 0.1:
+                    #Calc landscape score
+                    neighbors = 0
+                    neighborsSum = 0
+                    for m in range(-1,2):
+                        for n in range(-1,2):
+                            if m!=0 or n!= 0:
+                                if 0<=i+m < Agent.dim and 0 <= j+n <Agent.dim:
+                                    neighborsSum += env[a + k][b+l]
+                                    neighbors += 1
+                    neighborsAvg = neighborsSum/neighbors
+                    if neighborsAvg < minScore:
+                        minScore = neighborsAvg
+                        optimal = (i,j)
+        
+        # There was no flat terrain in the board, but this would only happen with a 1x1 or a 2x2
+        if optimal != None:
+            curr = optimal
+
+        # Find whether we're 5 distance if target not found for part 2
+        if not tileFound and part2:
+            updatePart2(env, Agent, curr)
+        
+        tileFound = checkTile(env, Agent, curr)
+        probabilitiesUpdate(Agent, curr, -1/Agent.dim**2, not part2)
+        tilePrev = (0, curr)
+
+    else:
+        # Takes the top cell and put it back
+        curr = Agent.probabilities.get()
+        Agent.probabilites.put(curr)
+
+        # Find whether we're 5 distance if target not found for part 2
+        if not tileFound and part2:
+            updatePart2(env, Agent, curr[1])
+            #Idt used
+        
+        tileFound = checkTile(env, Agent, curr[1])
+        probabilitiesUpdate(Agent, curr, Agent.beliefState[curr[1][0]][curr[1][1]], not part2)
+        
+        # Update prev cell
+        tilePrev = curr
+
+    while not tileFound:
+        if param == "Rule1" or param == "Rule2":
+            curr = Agent.probabilities.get()
+            Agent.probabilities.put(curr)
+            if not tileFound and part2:
+                updatePart2(env, Agent, curr[1])
+            tilesSearched += 1
+            tileFound = checkTile(env, Agent, curr[1])
+            probabitiesUpdate(Agent, curr[1], curr[0], not part2)
+        elif param == "Agent1" or param == "Agent2" or param == "Agent3" or param == "improved":
+            curr = Agent.probabilities.get()
+            Agent.probabilities.put(curr)
+            popped = [curr]
+            while True and not Agent.probabilities.empty():
+                temp = probablities.get()
+                if temp[0] != curr[0]:
+                    Agent.probabilities.put(temp)
+                    break
+                else:
+                    popped.append(temp)
+            minDist = Agent.dim * 2
+            minDistTile = None
+            for tile in popped:
+                if calcDist(tile[1], tilePrev[1]) < minDist:
+                    minDist = calcDist(tile[1], tilePrev[1])
+                    minDistTile = tile[1]
+            chosen = None
+            for tile in popped:
+                if tile[1] == minDistTile:
+                    chosen = tile
+                else:
+                    Agent.probabilities.put(tile)
+
+            curr = chosen
+            Agent.probabilities.put(curr)
+            if not tileFound and part2:
+                updatePart2(curr[1])
+            tilesTravelled += calcDist(curr[1], prevTile[1])
+
+    
+
+startSearch(env, SimpleAgent, "Agent1", False)
+
+class Landscape:
     def __init__(self, d):
 
         self._d: int = d
@@ -24,39 +264,11 @@ class Landscape:
         self._belief_states: List[List[float]] = [[1 / (self._d * self._d) for _ in range(self._d)] for _ in
                                                   range(self._d)]
         self._probability_queue: PriorityQueue[Tuple[float, Tuple[int, int]]] = self._initialize_probability_queue()
-        self.target = self._pick_random_cell()
+        self.target = [random.randint(0, self._d - 1), random.randint(0, self._d - 1)]
         self.proximity_history: List[List[float]] = [[0 for _ in range(self._d)] for _ in
                                                   range(self._d)]
         self.most_recent_searches = Queue(10)
-
-    def _change_target(self) -> None:
-        prev_target = self.target
-        successful_move = False
-        new_target = None
-
-        while not successful_move:
-            move_flag = random.randint(0, 3)
-            # Go up
-            if move_flag == 0:
-                new_target = (prev_target[0], prev_target[1]-1)
-
-            # Go down
-            elif move_flag == 1:
-                new_target = (prev_target[0], prev_target[1]+1)
-
-            # Go left
-            if move_flag == 2:
-                new_target = (prev_target[0]-1, prev_target[1])
-
-            # Go right
-            elif move_flag == 3:
-                new_target = (prev_target[0]+1, prev_target[1] + 1)
-
-            if new_target[0] >= 0 and new_target[1] < self._d:
-                successful_move = True
-
-        self.target = new_target
-
+        
 
     def generate_landscape(self) -> List[List[float]]:
         """
@@ -143,8 +355,26 @@ class Landscape:
             total_probability_sum += self._belief_states[curr_cell[1][0]][curr_cell[1][1]]
 
         # Recreate the probability_queue with updated values
-        self._probability_queue = self._normalize_belief_states(total_probability_sum, updated_queue, observed_cell,
-                                                                version, part2_flag)
+        scaling_factor = 1 / total_probability_sum
+        for i in range(self._d):
+            for j in range(self._d):
+                if version == "rule_1" or version == "rule_2" or version == "agent_1" or version == "agent_2":
+                    self._belief_states[i][j] = self._belief_states[i][j] * scaling_factor
+                    if not part2_flag:
+                        updated_queue.put((-1 * self._belief_states[i][j], (i, j)))
+                    else:
+                        priority = -1 * (self._belief_states[i][j] + self.proximity_history[i][j])
+                        updated_queue.put((priority, (i, j)))
+
+                # NOTE the version == "improved" if condition should rly go here but agent_3 is kind of wack
+                elif version == "agent_3" or version == "improved":
+                    self._belief_states[i][j] = self._belief_states[i][j] * scaling_factor
+                    if not part2_flag:
+                        updated_queue.put(((self._calculate_distance(observed_cell, (i, j)) + 1) / self._belief_states[i][j], (i, j)))
+                    else:
+                        priority = (self._calculate_distance(observed_cell, (i, j)) + 1) / self._belief_states[i][j] + self.proximity_history[i][j]
+                        updated_queue.put((priority, (i, j)))
+        self._probability_queue = updated_queue
 
     def _calculate_new_probability(self, curr_cell: Tuple[float, Tuple[int, int]], observed_cell: Tuple[int, int],
                                    observed_prior: float, flag: str):
@@ -206,7 +436,22 @@ class Landscape:
         self.most_recent_searches.put((curr_cell, within_5_from_target))
 
         # Move target
-        self._change_target()
+        targetOld = self.target
+        targetNew = NULL
+        while True:
+            rInt = random.randint(0, 3)
+            if rInt == 0:
+                targetNew = (targetOld[0], targetOld[1]-1)
+            elif rInt == 1:
+                targetNew = (targetOld[0], targetOld[1]+1)
+            if rInt == 2:
+                targetNew = (targetOld[0]-1, targetOld[1])
+            elif rInt == 3:
+                targetNew = (targetOld[0]+1, targetOld[1]+1)
+            if targetNew[0] >=0 and targetNew[1] < self._d:
+                break
+        self.target = targetNew
+
 
     def begin_search(self, version: str, part2_flag=None) -> Tuple[int, int, int]:
 
@@ -225,11 +470,11 @@ class Landscape:
 
         # Pick first cell, random if for rule 1 or agent 1
         if version == "rule_1" or version == "agent_1":
-            curr_cell = self._pick_random_cell()
-            print("Current cell", curr_cell)
-            print("Target cell", self.target)
-            print(search_count)
-            print(move_count)
+            curr_cell = [random.randint(0, self._d - 1), random.randint(0, self._d - 1)]
+            # print("Current cell", curr_cell)
+            # print("Target cell", self.target)
+            # print(search_count)
+            # print(move_count)
             target_found = self._search_cell(curr_cell)
 
             # Find whether we're 5 distance if target not found for part 2
@@ -251,10 +496,10 @@ class Landscape:
             if not target_found and part2_flag:
                 self._update_part_2(curr_cell)
 
-            print("Current cell", curr_cell)
-            print("Target cell", self.target)
-            print(search_count)
-            print(move_count)
+            # print("Current cell", curr_cell)
+            # print("Target cell", self.target)
+            # print(search_count)
+            # print(move_count)
             target_found = self._search_cell(curr_cell)
             self._update_probability_queue(curr_cell, -1 / self._d ** 2, version, part2_flag)
             prev_cell = (0, curr_cell)
@@ -291,10 +536,10 @@ class Landscape:
                 if not target_found and part2_flag:
                     self._update_part_2(curr_cell[1])
 
-                print("Current cell", curr_cell)
-                print("Target cell", self.target)
+                # print("Current cell", curr_cell)
+                # print("Target cell", self.target)
                 search_count += 1
-                print(search_count)
+                # print(search_count)
                 target_found = self._search_cell(curr_cell[1])
                 self._update_probability_queue(curr_cell[1], curr_cell[0], version, part2_flag)
                 self.print_belief_states()
@@ -408,40 +653,6 @@ class Landscape:
             for j in range(self._d):
                 self._belief_states[i][j] = self._belief_states[i][j] * scaling_factor
                 updated_queue.put((-1 * self._belief_states[i][j], (i, j)))
-
-        return updated_queue
-
-    def _normalize_belief_states(self, total_probability_sum: int, updated_queue: PriorityQueue,
-                                 observed_cell: Tuple[int, int], version: str, part2_flag: bool):
-        """
-        Scale the probability values so the total probability is 1
-        :param total_probability_sum:
-        :param updated_queue:
-        :return:
-        """
-
-        scaling_factor = 1 / total_probability_sum
-        print("scaling: ", scaling_factor)
-        for i in range(self._d):
-            for j in range(self._d):
-                if version == "rule_1" or version == "rule_2" or version == "agent_1" or version == "agent_2":
-                    self._belief_states[i][j] = self._belief_states[i][j] * scaling_factor
-
-                    if not part2_flag:
-                        updated_queue.put((-1 * self._belief_states[i][j], (i, j)))
-                    else:
-                        priority = -1 * (self._belief_states[i][j] + self.proximity_history[i][j])
-                        updated_queue.put((priority, (i, j)))
-
-                # NOTE the version == "improved" if condition should rly go here but agent_3 is kind of wack
-                elif version == "agent_3" or version == "improved":
-                    self._belief_states[i][j] = self._belief_states[i][j] * scaling_factor
-
-                    if not part2_flag:
-                        updated_queue.put(((self._calculate_distance(observed_cell, (i, j)) + 1) / self._belief_states[i][j], (i, j)))
-                    else:
-                        priority = (self._calculate_distance(observed_cell, (i, j)) + 1) / self._belief_states[i][j] + self.proximity_history[i][j]
-                        updated_queue.put((priority, (i, j)))
 
         return updated_queue
 
